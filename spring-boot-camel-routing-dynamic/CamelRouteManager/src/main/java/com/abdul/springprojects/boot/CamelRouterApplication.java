4a1bffea2d6f25343c0480fd14bb1f97a1649728
abdulgafurinfo <abdulgafurinfo@gmail.com>
2018-04-28 23:49:50 +0530

    Dynamic config

--- spring-boot-camel-routing-dynamic/CamelRouteManager/src/main/java/com/abdul/springprojects/boot/CamelRouterApplication.java
+++ spring-boot-camel-routing-dynamic/CamelRouteManager/src/main/java/com/abdul/springprojects/boot/CamelRouterApplication.java
@@ -2,6 +2,9 @@ package com.abdul.springprojects.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.abdul.springprojects.util.GlobalCamelContext;
 
 @SpringBootApplication
 public class CamelRouterApplication {
@@ -9,4 +12,12 @@ public class CamelRouterApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CamelRouterApplication.class, args);
 	}
+	
+	
+	@Bean
+	public GlobalCamelContext camelInvoker() {
+		return new GlobalCamelContext();
+	}
+	
+	
 }
---
+++ spring-boot-camel-routing-dynamic/CamelRouteManager/src/main/java/com/abdul/springprojects/boot/rest/EndPointBuilder.java
@@ -0,0 +1,53 @@
+package com.abdul.springprojects.boot.rest;
+
+import java.util.List;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class EndPointBuilder extends RouteBuilder {
+
+	String fileEndPoint = null;
+	String targetEndPoint = null;
+	ServiceConfig serviceConfig = null;
+
+	public boolean resolve(ServiceConfig serviceConfig) {
+		this.serviceConfig = serviceConfig;
+		this.fileEndPoint = remoteEndPoint(serviceConfig);
+		this.targetEndPoint = targetEndPoint(serviceConfig);
+		return false;
+
+	}
+
+	public String remoteEndPoint(ServiceConfig serviceConfig) {
+
+		fileEndPoint = serviceConfig.getRemoteEndPoint() + ":" + serviceConfig.getRemoteDir() + "?" + "autoCreate=true";
+		return fileEndPoint;
+
+	}
+
+	public String targetEndPoint(ServiceConfig serviceConfig) {
+
+		//targetEndPoint = "activemq" + ":" + "queue" + ":" + serviceConfig.getTargetDestination();
+		targetEndPoint =  serviceConfig.getRemoteEndPoint() + ":" + serviceConfig.getRemoteDir() +"a" + "?" + "autoCreate=true";
+		return targetEndPoint;
+
+	}
+	
+
+	@Override
+	public void configure() throws Exception {
+
+		from(fileEndPoint).log("{body}").to(targetEndPoint).routeId(serviceConfig.getServiceId());
+
+		 
+	}
+
+	public String getFileEndPoint() {
+		return fileEndPoint;
+	}
+
+	public void setFileEndPoint(String fileEndPoint) {
+		this.fileEndPoint = fileEndPoint;
+	}
+
+}
